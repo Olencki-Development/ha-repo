3c04ff05cc79015c5c6696ba430b79cc5463e01d
Brian Olencki <bolencki13@users.noreply.github.com>
2022-09-15 17:42:21 -0400

    Add error page

---
+++ ha-portal/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<div class="flex justify-center items-center h-screen w-full">
+  <div>
+    <h1>{$page.status}: {$page.error.message}</h1>
+    <pre>{$page.error.stack}</pre>
+  </div>
+</div>
