4003cf32c98b9dcf294518d8b9b203bc43f99f06
Brian Olencki <brian@olencki.com>
2022-09-15 07:19:15 -0400

    Remove redundent api call with layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,8 +3,11 @@ import { notifications } from '$stores/notifications';
 import { NotificationWithActions } from '$lib/app/models/NotificationWithActions';
 import { z } from 'zod';
 
-export const load: LayoutLoad = async function ({ data }) {
-	notifications.set(z.array(NotificationWithActions).parse(data.notifications));
+export const load: LayoutLoad = async function ({ data, fetch }) {
+	const response = await fetch('/notifications');
+	const json = await response.json();
+
+	notifications.set(z.array(NotificationWithActions).parse(json));
 
 	return {
 		notifications
